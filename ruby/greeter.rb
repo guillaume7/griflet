98cbf3ac5fc45869fd4d506f0c6d673e9e821aa1
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-05-07 23:09:55 +0000

    Added comments, used the attr_accessor, tested it in simple code that makes use of the simple hello world class.

--- ruby/greeter.rb
+++ ruby/greeter.rb
@@ -1,19 +1,35 @@
 #! /usr/bin/env ruby
 
 class Greeter
+    attr_accessor :name
+
+    # Create the object
     def initialize(name = "world")
         @name = name.capitalize
     end
+
+    # Say hi
     def say_hi
         puts "Hi #{@name}!"
     end
+
+    #Say bye
     def say_bye
         puts "Bye #{@name}, come back soon."
     end
+
 end
 
 if __FILE__ == $0
     greet = Greeter.new("griflet")
+    greet.say_hi
+    greet.say_bye
+
+    greet.name="xana"
+    greet.say_hi
+    greet.say_bye
+
+    greet = Greeter.new("xana")
     greet.say_hi
     greet.say_bye
 end
