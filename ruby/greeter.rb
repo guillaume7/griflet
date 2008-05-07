128a5fc356fffde658e9537747f7534f3aa27a4b
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-05-07 23:46:49 +0000

    Added list functionality to class.w

--- ruby/greeter.rb
+++ ruby/greeter.rb
@@ -1,21 +1,43 @@
 #! /usr/bin/env ruby
 
 class Greeter
-    attr_accessor :name
+    attr_accessor :names
 
     # Create the object
-    def initialize(name = "world")
-        @name = name.capitalize
+    def initialize(names = "world")
+        if names.respond_to?("capitalize")
+            @names = names.capitalize
+        else
+            @names = names
+        end
     end
 
-    # Say hi
+    # Say hi to everybody
     def say_hi
-        puts "Hi #{@name}!"
+        if @names.nil?
+            puts "..."
+        else 
+            if @names.respond_to?("each")
+                @names.each do |name|
+                    puts "Hello #{name.capitalize}!"
+                end
+            else
+                puts "Hi #{@names}!"
+            end
+        end
     end
 
     #Say bye
     def say_bye
-        puts "Bye #{@name}, come back soon."
+        if @names.nil?
+            puts "..."
+        else 
+            if @names.respond_to?("join")
+                puts "Bye #{@names.join(", ")}, come back soon!"
+            else
+                puts "Bye #{@names.capitalize}, come back soon!"
+            end
+        end
     end
 
 end
@@ -25,11 +47,23 @@ if __FILE__ == $0
     greet.say_hi
     greet.say_bye
 
-    greet.name="xana"
+    # Change name to "xana"
+    greet.names="xana"
     greet.say_hi
     greet.say_bye
 
+    # Create new object with "Xana"
     greet = Greeter.new("xana")
+    greet.say_hi
+    greet.say_bye
+
+    # Change name for a swarm of names!
+    greet = Greeter.new(["griflet", "cepheus", "macleod", "guimas"])
+    greet.say_hi
+    greet.say_bye
+
+    # Change to nil
+    greet.names = nil
     greet.say_hi
     greet.say_bye
 end
