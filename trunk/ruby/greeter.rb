4af477c9c1a290f7ea6b2710d6cf9750024d898a
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-05-11 17:15:07 +0000

    Well, added three ruby scripts that interface with xmpp4r. These were originally created by Dominiek and can be downloaded from his blog @http://dominiek.com/articles/2008/2/15/how-to-build-a-twitter-agent.
    I really hope to learn ruby and the xmpp4r library, as i'm going to need 
    that badly in the near future. The bad thing is that it's rather 
    complex, in the opposite to the twitter REST api which is really the 
    simplest thing out there...

--- trunk/ruby/greeter.rb
+++ trunk/ruby/greeter.rb
@@ -1,4 +1,12 @@
 #! /usr/bin/env ruby
+#=Greeter class
+#
+#This class aims at illustrate how to build a cute nice little hello world program.
+#I hope you enjoy it
+#==initialize
+#==say_hi
+#==say_bye
+#==ask_how
 
 class Greeter
     attr_accessor :names
@@ -40,30 +48,50 @@ class Greeter
         end
     end
 
+    #Ask how are you
+    def ask_feel
+        if @names.nil?
+            puts "..."
+        else
+            if @names.respond_to?("each")
+                @names.each do |name|
+                    puts "Hey #{name.capitalize}, how do you feel?"
+                end
+            else
+                puts "Hey #{@names.capitalize}, how do you feel?"
+            end
+        end
+    end
+
 end
 
 if __FILE__ == $0
     greet = Greeter.new("griflet")
     greet.say_hi
     greet.say_bye
+    greet.ask_feel
 
     # Change name to "xana"
     greet.names="xana"
     greet.say_hi
     greet.say_bye
+    greet.ask_feel
 
     # Create new object with "Xana"
     greet = Greeter.new("xana")
     greet.say_hi
     greet.say_bye
+    greet.ask_feel
 
     # Change name for a swarm of names!
     greet = Greeter.new(["griflet", "cepheus", "macleod", "guimas"])
     greet.say_hi
     greet.say_bye
+    greet.ask_feel
 
     # Change to nil
     greet.names = nil
     greet.say_hi
     greet.say_bye
+    greet.ask_feel
 end
