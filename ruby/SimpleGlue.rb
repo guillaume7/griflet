8f5a8d9cec32a9985d53afbaa57e9a01c3e3da60
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-06-03 12:21:18 +0000

    Created a simple list of examples on how to properly use the RootTransform.rb classes

---
+++ ruby/SimpleGlue.rb
@@ -0,0 +1,25 @@
+require "roottransform"
+
+#Batch glue action
+glue = IsTheActionToPerformWithFiles.new("root_ConvertToHdf5Glue.dat", "gluelist.txt")
+glue.usesTransformedFilesNamedAfter("GlueAction")
+glue.usesAConverttohdf5BatchfileNamed("GlueAllHdf5.bat")
+glue.ing
+
+#Batch patch action
+patch = IsTheActionToPerformWithFiles.new("root_ConvertToHdf5patch.dat", "patchlist.txt")
+patch.usesTransformedFilesNamedAfter("patchAction")
+patch.usesAConverttohdf5BatchfileNamed("patchAllHdf5.bat")
+patch.ing
+
+#Batch convert2Netcdf action
+ncconvert = IsTheActionToPerformWithFiles.new("root_Convert2Netcdf.dat", "ncconvertlist.txt")
+ncconvert.usesTransformedFilesNamedAfter("Convert2Netcdf")
+ncconvert.usesAConvert2netcdfBatchfileNamed("ncconvertAllHdf5.bat", )
+ncconvert.ing
+
+#Batch extract action
+extract = IsTheActionToPerformWithFiles.new("root_myHDF5Extract.dat", "extractlist.txt")
+extract.usesTransformedFilesNamedAfter("HDF5Extract")
+extract.usesAGenericMohidtoolBatchfile("extractAllHdf5.bat", "HDF5Extractor.exe", "myHDF5extract.dat")
+extract.ing
