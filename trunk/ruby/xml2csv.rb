f92ee11259d1a7c0cceae583a6179770d046e59b
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-11-15 11:34:43 +0000

    made the api caching work

--- trunk/ruby/xml2csv.rb
+++ trunk/ruby/xml2csv.rb
@@ -2,22 +2,33 @@ require 'open-uri'
 require 'rexml/document'
 include REXML
 
+str = ''
 begin
-txt = open('http://codebits.sapo.pt/op/getusermap/')
-#puts txt.read
-doc = Document.new(txt.read)
-doc.elements.each("*/user") { |element|
-    str = ''
-    element.attributes.each { |key,val|
-        str = str + val + '; '
+    txt = open('http://codebits.sapo.pt/op/getusermap/')
+    #puts txt.read
+    doc = Document.new(txt.read)
+    doc.elements.each("*/user") { |element|
+        element.attributes.each { |key,val|
+            str = str + val + '|'
+        }
+        element.elements.each { |item|
+            if item.has_text?
+                str = str + '|' + item.get_text.to_s
+            end
+        }
+        str = str + "\n"
     }
-    element.elements.each { |item|
-        if item.has_text?
-            str = str + '; ' + item.get_text.to_s
+    puts str
+    begin
+        if str.match(/guillaume/i)
+            out = File.open('rfidbits.txt', 'w')
+            out.write(str)
+        else
+            puts "Hmmm something's fishy around here!\n"
         end
-    }
-    puts str + "\n"
-}
+    rescue
+        puts "Couldn't open rfidbits.txt"
+    end
 
 rescue
     $stderr.print "uri IO failed: " + $! + "\n"
