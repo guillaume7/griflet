fd16bcce38a95cbe55cdcf4d2f091ff31553d2f2
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2008-11-17 14:34:24 +0000

    made some changes

--- ruby/grep_file.rb
+++ ruby/grep_file.rb
@@ -1,35 +1,43 @@
 require 'open-uri'
 
-def grep_file(pattern, file="yahoofinance.txt")
+class Grepfile
+
+    def initialize(file="yahoofinance.txt")
+        @file=file
+    end
+
+    def grep(pattern)
 #    puts file
 #    puts pattern
-    begin
-        results = Array.new
-        File.open(file, 'r') do |fs|
-            while line = fs.gets
-                if line.match(pattern)
-                    results.push(line.chomp)
-                else
-#                    puts "Line #{line.chomp} doesn't match"
+        begin
+            results = Array.new
+            File.open(@file, 'r') do |fs|
+                while line = fs.gets
+                    if line.match(pattern)
+                        results.push(line.chomp)
+                    else
+    #                    puts "Line #{line.chomp} doesn't match"
+                    end
                 end
             end
+
+            if results.nil?
+                "WTF means #{pattern}?\n"
+            else
+                results
+            end
+
+        rescue SystemCallError
+             $stderr.print "IO failed: " + $! + "\n"
+             "WTF! IO failed " + $! + "\n"
+    
         end
-
-        if results.nil?
-            "WTF means #{pattern}?\n"
-        else
-            results
-        end
-
-    rescue SystemCallError
-         $stderr.print "IO failed: " + $! + "\n"
-         "WTF! IO failed " + $! + "\n"
-
     end
 
 end
 
 if __FILE__ == $0 
-    puts grep_file(/[A-Z]/, 'stickers.txt')
+    finance = Grepfile.new
+    puts finance.grep(/[A-Z]/)
 
 end
