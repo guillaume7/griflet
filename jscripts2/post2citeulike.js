a2b5bcf99eba5070652781150c90f9809bfe3750
guillaume.riflet <guillaume.riflet@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2009-02-20 16:53:36 +0000

    putting revision 67 on top

--- jscripts2/post2citeulike.js
+++ jscripts2/post2citeulike.js
@@ -1,7 +1,5 @@
 if(typeof(Citeulike) == 'undefined') Citeulike = {}
-<a onmouseout="document.images['addEmail'].src='/scidirimg/sci_dir/emailarticle_a.gif'" onmouseover="document.images['addEmail'].src='/scidirimg/sci_dir/emailarticle_b.gif'" style="vertical-align: bottom; color: rgb(0, 0, 0); font-family: arial,verdana,helvetica,sans-serif; text-decoration: none;" href="/science?_ob=EmailFriendURL&_method=gatherInfo&_ArticleListID=869795052&refSource=html&count=1&_uoikey=B6VCR-4T9CCSY-2&_acct=C000057394&_version=1&_userid=2459750&md5=517beb052ed9a2be270d78071a27cebc">
-  E-mail Article
-</a>
+
 Citeulike.Insert = {
 
    go: function() { 
@@ -10,8 +8,8 @@ Citeulike.Insert = {
             for (var j = 0, u; u = o.attributes[j]; j++) {
                 if( u.name == "title" && u.value == "Article Toolbox" ){
                     var anchor = document.createElement('a');
-                    anchor.href = 'http://test.com';
-                    anchor.innerHTML = 'Post';
+                    anchor.href = 'http://test.com';    
+                    anchor.innerHTML = 'listen to me Rock!!';
                     var entry = document.createElement('td');
                     entry.appendChild(anchor);
                     var line = document.createElement('tr');
