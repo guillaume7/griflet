8170d8be9e37bc5400b258e5a8c150bbba807a4c
guillaume.riflet@gmail.com <guillaume.riflet@gmail.com@e600c982-ab4c-0410-9e4c-894b8adec9a3>
2011-07-31 01:34:13 +0000

    Adicionar o meu primeiro codigo fortran2003: a classe Colecao que se encontra na pasta 'paises'

---
+++ trunk/fortran/paises/colecao.F90
@@ -0,0 +1,222 @@
+module classColecao
+
+  implicit none
+
+  private
+
+  public C_Colecao
+
+  type C_Colecao
+
+    integer                         :: id = 1
+
+    class(C_Colecao), pointer       :: fundador => null()
+
+    class(C_Colecao), pointer       :: seguinte => null()
+
+  contains
+
+    procedure                       :: adicionar => adicionar
+
+    procedure                       :: remover => remover
+
+    procedure                       :: removerTudo => removerTudo
+ 
+    procedure                       :: obter => obter
+
+    procedure                       :: obterPrimeiro => obterPrimeiro
+
+    procedure                       :: obterSeguinte => obterSeguinte
+ 
+    procedure                       :: obterAnterior => obterAnterior
+
+    procedure                       :: obterUltimo => obterUltimo
+
+  end type C_Colecao
+
+contains
+
+  subroutine adicionar(self)
+
+    class(C_Colecao), target      :: self
+
+    class(C_Colecao), pointer     :: new
+
+    integer                       :: id
+
+    new => self.obterUltimo()
+
+    allocate(new%seguinte)
+
+    id = new%id
+
+    new => new%seguinte
+
+    new%id = id + 1
+
+  end subroutine adicionar
+
+  subroutine remover(self)
+
+    class(C_Colecao)             :: self
+
+    class(C_Colecao), pointer    :: ptr
+
+    ptr => self.obterUltimo()
+
+    deallocate(ptr)
+
+  end subroutine remover
+
+  subroutine removerTudo(self)
+
+    class(C_Colecao)              :: self
+
+    class(C_Colecao), pointer     :: ptr, first
+
+    first => self.obterPrimeiro();
+
+    do while ( associated ( first ) )
+
+      call first.remover()
+
+    end do
+
+  end subroutine removerTudo
+
+  function obter(self,id) result(nodo)
+
+    class(C_Colecao)              :: self
+
+    integer                       :: id
+
+    class(C_Colecao), pointer     :: nodo
+
+    nodo => self%fundador
+
+    do while ( nodo%id .ne. id )
+
+      if ( associated( nodo%seguinte ) ) then
+
+        nodo => nodo%seguinte
+
+      else
+
+        id = nodo%id
+
+        write(*,*) 'WARN 000: Nao se encontrou o nodo ', id, ' na colecao.'
+
+      end if
+
+    end do
+
+  end function obter
+
+  function obterPrimeiro(self) result(primeiro)
+
+    class(C_Colecao)              :: self
+
+    class(C_Colecao), pointer     :: primeiro
+
+    primeiro => self%fundador
+
+  end function obterPrimeiro
+
+  function obterAnterior(self) result(anterior)
+
+    class(C_Colecao)              :: self
+
+    class(C_Colecao), pointer     :: anterior, seguinte
+
+    integer                       :: id
+
+    anterior => self%fundador
+
+    if ( self%id .ne. anterior%id ) then
+
+      seguinte => anterior%seguinte
+
+      id = seguinte%id
+
+      do while ( id .ne. self%id )
+
+        if ( associated( seguinte%seguinte ) ) then
+      
+          anterior => anterior%seguinte
+
+          seguinte => seguinte%seguinte
+
+          id = seguinte%id
+
+        else
+
+          id = self%id
+
+          write(*,*) 'WARN 001: Nao foi encontrado o nodo anterior na colecao'
+          write(*,*) 'Colecao corrompida.'
+
+        endif
+
+      end do
+
+    endif
+
+  end function obterAnterior
+
+  function obterSeguinte(self) result(seguinte)
+
+    class(C_Colecao), target      :: self
+
+    class(C_Colecao), pointer     :: seguinte
+
+    seguinte => self
+
+    if ( associated( self%seguinte ) ) then
+
+      seguinte => self%seguinte
+
+    end if
+
+  end function obterSeguinte
+
+  function obterUltimo(self) result(ultimo)
+
+    class(C_Colecao), target      :: self
+
+    class(C_Colecao), pointer     :: ultimo
+
+    ultimo => self
+
+    do while ( associated( ultimo%seguinte ) )
+
+      ultimo => ultimo%seguinte
+
+    end do
+
+  end function obterUltimo
+
+end module classColecao
+
+!----------------- UnitTests of the class -----------------------------
+
+program colecao
+
+  use classColecao
+
+  implicit none
+
+  type(C_Colecao) :: list
+
+  integer         :: i
+
+  list = C_Colecao(1)
+  
+  do i = 1, 3
+
+    call list.adicionar()
+
+  end do
+
+  call list.removerTudo()
+
+end program colecao
---
+++ trunk/fortran/paises/mapaMundo.F90
@@ -0,0 +1,327 @@
+        module moduleMapaMundo
+
+        implicit none
+
+        private
+
+        !Declaração do alcance das subrotinas
+        !Rotinas que mexem com o tipo "T_Pais"
+        private :: fundarPais
+        private :: explorarPais
+        !Rotinas que mexem com o tipo "T_Mapa"
+        public :: descobrirNovoMundo
+        private :: criarMapa
+        private :: viajarFimDoMundo
+        private :: viagemRegresso
+        private :: viajarSeguinte
+        private :: viajarAnterior
+        public :: verMapaMundo
+        public :: verMapa
+        public :: procurarMapa
+        public :: abandonarNovoMundo
+        public :: apocalipse
+
+        !Declaração de tipos e variáveis globais do módulo
+        public :: StringLength
+        integer, parameter  :: StringLength = 128
+        
+        public :: T_Pais
+        type T_Pais
+                character(len=StringLength) :: nome
+                character(len=StringLength) :: capital
+                real                        :: pop
+                real                        :: pib
+        end type T_Pais
+
+        public :: T_Mapa
+        type T_Mapa
+                type(T_Pais)            :: pais
+                type(T_Mapa), pointer   :: mapaDeCasa => Null()
+                type(T_Mapa), pointer   :: mapaSeguinte => Null()
+        end type T_Mapa
+
+        type(T_Mapa), pointer          :: Me => Null()
+
+        contains
+
+        subroutine fundarPais(pais)
+
+                type(T_Pais) :: pais
+
+                write(*,*) 'Insira o nome do pais'
+                read(*,*) pais%nome
+
+                write(*,*) 'Insira a sua capital'
+                read(*,*) pais%capital
+
+                write(*,*) 'Insira a populacao em milhoes de habitantes'
+                read(*,*) pais%pop
+
+                write(*,*) 'Insira o PIB em milhoes de euros'
+                read(*,*) pais%pib
+
+                write(*,*) ''
+
+        end subroutine fundarPais
+
+        subroutine explorarPais(pais)
+
+                type(T_Pais)            :: pais
+
+                write(*,*) 'O pais ', trim(pais%nome),','
+                write(*,*) 'cuja capital eh ', trim(pais%capital),','
+                write(*,*) 'tem uma populacao de', pais%pop,'habitantes'
+                write(*,*) 'e um pib de ', pais%pib, 'milhoes de euros.'
+
+        end subroutine explorarPais
+
+        subroutine descobrirNovoMundo()
+
+                !Se o mapa ainda não existe, então é criado
+                if (.not. associated(Me)) then
+
+                        call criarMapa(Me,Me)
+
+                !Senão o Atlas é incrementado
+                else
+
+                        call viajarFimDoMundo
+
+                        call criarMapa(Me%mapaSeguinte, Me%mapaDeCasa)
+
+                        Me => Me%mapaSeguinte
+
+                end if
+
+                call fundarPais(Me%pais)
+
+        end subroutine descobrirNovoMundo
+
+        subroutine viajarFimDoMundo()
+
+                do while (associated(Me%mapaSeguinte))
+
+                        Me => Me%mapaSeguinte
+
+                end do  
+
+        end subroutine viajarFimDoMundo
+
+        subroutine viagemRegresso()
+
+                if(associated(Me)) then
+
+                    Me => Me%mapaDeCasa
+
+                endif
+
+        end subroutine viagemRegresso
+        
+        subroutine viajarSeguinte()
+
+            if(associated(Me)) then
+
+                if(associated(Me%mapaSeguinte)) then
+
+                    Me => Me%mapaSeguinte
+
+                endif
+
+            endif
+
+        end subroutine
+
+        subroutine viajarAnterior()
+
+            character(len=StringLength) :: nomePais
+
+            if(associated(Me)) then
+
+                nomePais = Me%pais%nome
+
+                call viagemRegresso()
+
+                if (nomePais .ne. Me%pais%nome) then
+                
+                    do while (  associated(Me%mapaSeguinte)                 &
+                                .and.                                       &
+                                nomePais .ne. Me%mapaSeguinte%pais%nome)
+                    
+                            call viajarSeguinte()
+                        
+                    enddo
+                    
+                endif
+
+            endif
+
+        end subroutine viajarAnterior
+
+        subroutine criarMapa(mapa, mapaDeCasa)
+
+                type(T_Mapa), pointer :: mapa, mapaDeCasa
+
+                allocate(mapa)
+
+                mapa%mapaDeCasa => mapaDeCasa
+
+        end subroutine criarMapa
+
+        subroutine abandonarNovoMundo
+ 
+                type(T_Mapa), pointer :: mundoAbandonado, novoFimDoMundo
+
+                if (associated(Me)) then
+
+                    call viajarFimDoMundo
+
+                    mundoAbandonado => Me
+                    
+                    call viajarAnterior
+                    
+                    novoFimDoMundo => Me
+                    
+                    if (associated(novoFimDoMundo%mapaSeguinte)) then
+                    
+                        nullify(novoFimDoMundo%mapaSeguinte)
+                        
+                    endif
+
+                    call viagemRegresso
+
+                    !Testa se o mundo abandonado é casa...
+                    nullify(mundoAbandonado%mapaDeCasa)
+
+                    if (not(associated(Me%mapaDeCasa))) then
+
+                        nullify(Me)
+
+                    endif
+
+                    deallocate(mundoAbandonado)
+
+                    if (not(associated(Me))) then
+
+                        write(*,*) 'Mundo destruido.'
+
+                    endif
+
+                endif
+
+        end subroutine abandonarNovoMundo
+
+        subroutine apocalipse
+
+                do while (associated(Me))
+
+                    call abandonarNovoMundo
+
+                end do
+
+        end subroutine apocalipse
+
+        subroutine verMapaMundo
+
+                call viagemRegresso
+
+                write(*,*) trim(Me%pais%nome)
+
+                do while(associated(Me%mapaSeguinte))
+
+                        Me => Me%mapaSeguinte                        
+
+                        write(*,*) trim(Me%pais%nome)
+
+                end do
+
+        end subroutine verMapaMundo
+
+        subroutine verMapa
+
+                type(T_Mapa), pointer   :: presente
+
+                presente => Me
+
+                call viajarAnterior
+
+                if (Me%pais%nome .ne. presente%pais%nome) then
+
+                    write(*,*) trim(Me%pais%nome)
+
+                else
+
+                    write(*,*) 'Casa.'
+
+                endif
+
+                Me => presente
+
+                write(*,*) trim(Me%pais%nome)
+
+                if (associated(Me%mapaSeguinte)) then
+
+                        write(*,*) trim(Me%mapaSeguinte%pais%nome)
+
+                else
+                        
+                        write(*,*) 'Fim do Mundo.'
+
+                endif
+
+        end subroutine verMapa
+
+        subroutine procurarMapa(nome)
+
+                character(len=StringLength)     :: nome
+
+                type(T_Mapa), pointer           :: esteMapa => Null()
+
+                Me => Me%mapaDeCasa
+
+                do while(associated(Me%mapaSeguinte))
+
+                        if (trim(nome) .eq. trim(Me%pais%nome)) then
+
+                                esteMapa => Me
+
+                        end if
+
+                        Me => Me%mapaSeguinte
+
+                end do
+
+                if (associated(esteMapa)) then
+
+                        Me => esteMapa
+
+                else
+
+                        Me => Me%mapaDeCasa
+
+                        write(*,*)'Não encontrou mapa de ', trim(nome), '.'
+                        write(*,*)'De regresso a casa, a ', trim(Me%pais%capital),' :('
+                end if
+
+        end subroutine procurarMapa
+
+        end module moduleMapaMundo
+
+        program mapaMundo
+        
+        use moduleMapaMundo
+
+        implicit none
+
+        integer i
+
+        do i = 1,3
+                call descobrirNovoMundo()
+        enddo
+
+        call verMapaMundo()
+
+        call apocalipse()
+
+        pause
+
+        end program mapaMundo
